973547e50270c2f393e94e2d119dfcb685aeb676
Mafendo <105345937+Mafendo@users.noreply.github.com>
2025-08-24 20:42:49 +0200

    completed paralax

--- Assets/Scripts/ParallaxEffect.cs
+++ Assets/Scripts/ParallaxEffect.cs
@@ -1,60 +1,36 @@
+
+
+using Unity.VisualScripting;
 using UnityEngine;
 
-[RequireComponent(typeof(SpriteRenderer))]
 public class ParallaxEffect : MonoBehaviour
 {
-    [SerializeField] private GameObject cam;
-    [SerializeField] private float parallaxEffectSpeed = 0.5f;
-    [SerializeField] private bool pixelSnap = true;
-    [SerializeField] private float pixelsPerUnit = 100f;
+    private float startPos;
+    private float length;
+    [SerializeField] GameObject cam;
+    [SerializeField] float ParallaxFactor;
 
-    private Vector2 startPos;
-    private Vector2 spriteSize;
 
-    void Start()
+
+    private void Start()
     {
-        startPos = transform.position;
-        SpriteRenderer sr = GetComponent<SpriteRenderer>();
-        spriteSize = sr.bounds.size;
+        startPos = transform.position.x;
+        length = GetComponent<SpriteRenderer>().bounds.size.x;
+
     }
 
-    void FixedUpdate()
+    private void Update()
     {
-        Vector2 cameraPos = cam.transform.position;
+        float temp = cam.transform.position.x * (1 - ParallaxFactor);
+        float distance = cam.transform.position.x * ParallaxFactor;
+       transform.position = new Vector3(startPos + distance, transform.position.y, transform.position.z);
 
-        Vector2 distance = cameraPos * parallaxEffectSpeed;
-        Vector2 movement = cameraPos * (1 - parallaxEffectSpeed);
+        
+         // If camera moved outside sprite, reposition
+        if (temp > startPos + length) startPos += length;
+        else if (temp < startPos - length) startPos -= length;
 
-        Vector2 newPos = startPos + distance;
 
-        // Optional: snap to pixel grid
-        if (pixelSnap)
-        {
-            float pixelSize = 1f / pixelsPerUnit;
-            newPos.x = Mathf.Round(newPos.x / pixelSize) * pixelSize;
-            newPos.y = Mathf.Round(newPos.y / pixelSize) * pixelSize;
-        }
 
-        transform.position = new Vector3(newPos.x, newPos.y, transform.position.z);
-
-        // Repeat (wrap) horizontally
-        while (movement.x > startPos.x + spriteSize.x)
-        {
-            startPos.x += spriteSize.x;
-        }
-        while (movement.x < startPos.x - spriteSize.x)
-        {
-            startPos.x -= spriteSize.x;
-        }
-
-        // Repeat (wrap) vertically (optional)
-        while (movement.y > startPos.y + spriteSize.y)
-        {
-            startPos.y += spriteSize.y;
-        }
-        while (movement.y < startPos.y - spriteSize.y)
-        {
-            startPos.y -= spriteSize.y;
-        }
     }
 }
